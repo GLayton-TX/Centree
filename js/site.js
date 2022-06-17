9d7aee02219c6f67f426501eb8c517daccc6a027
GLayton-TX <74160349+GLayton-TX@users.noreply.github.com>
2022-06-16 21:25:42 -0500

    Add JS
    Added JS for the app and completed customization.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World!");
+// get values from ui
+function getValues(){
+    let startVal = parseInt(document.getElementById("startVal").value);
+    let endVal = parseInt(document.getElementById("endVal").value);
+    if (!Number.isInteger(startVal) || !Number.isInteger(endVal)) {
+        alert("You must enter only Integers into both fields.")
+    }
+    let numbers = generateNumbers(startVal,endVal);
+    displayResults(numbers);
+}
+
+// generate numbers from start value to ending value
+function generateNumbers(startVal,endVal){
+    let numbers = [];
+    for (let i = startVal; i < endVal+1; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+//display the generated numbers, even numbers bold per challenge
+function displayResults(numbers){
+    let tableRow ="";
+    numbers.forEach(element => {
+        if (element % 2 === 0 && element != 0) {
+        tableRow += `<tr><td><b>${element}</b></td></tr>`;
+        }
+        else{
+        tableRow += `<tr><td>${element}</td></tr>`;
+        }
+    });
+    document.getElementById("results").innerHTML = tableRow
 }
